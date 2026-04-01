d2450250ffc754a7078d57802a1f6817fed2fe0c
Zach Benedict Hallare <zachhallare@gmail.com>
2026-04-01 17:35:48 +0800

    Backend and frontend have been initialized.

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 5000");
+});
+
